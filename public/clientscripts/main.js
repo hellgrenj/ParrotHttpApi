d4bb8091ad8e0e191639b7406d45c792d72b5097
Fredrik Karlsson <fredrik@Fredriks-MacBook-Pro.local>
2012-11-17 15:42:04 +0100

    Lagt till jQuery och main.js

---
+++ public/clientscripts/main.js
@@ -0,0 +1,14 @@
+// main.js
+$(function() {
+
+	if(typeof($('#cameraImage') != 'undefined ' && $('#cameraImage').length) {
+		setInterval(function({
+			var source = $('#cameraImage').prop('src');
+			$('#cameraImage').prop('src', source + '?' + rndNum);
+		}, 1000));
+	}
+
+	function rndNum() {
+		return Math.floor((Math.random()*10000)+1);
+	}
+});
