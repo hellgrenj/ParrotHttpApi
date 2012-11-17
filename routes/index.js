bd81da9bc83651054edc3e1369a11d668361ecdd
Fredrik Karlsson <fredrik@Fredriks-MacBook-Pro.local>
2012-11-17 14:21:22 +0100

    Lagt till knappar och style

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,34 @@
  * GET home page.
  */
 var arDrone = require('ar-drone');
+var http = require('http');
 var client  = arDrone.createClient();
+
+var pngStream = arDrone.createPngStream();
+
+var lastPng;
+
+// Fetch png stream and store in lastPng
+pngStream
+  .on('error', console.log)
+  .on('data', function(pngBuffer) {
+    lastPng = pngBuffer;
+  });
+
+var server = http.createServer(function(req, res) {
+  if (!lastPng) {
+    res.writeHead(503);
+    res.end('Did not receive any png data yet.');
+    return;
+  }
+
+  res.writeHead(200, {'Content-Type': 'image/png'});
+  res.end(lastPng);
+});
+
+server.listen(8080, function() {
+  console.log('Serving latest png on port 8080 ...');
+});
 
 exports.index = function(req, res){  
   res.render('index', { message: 'Parrot Index' });
